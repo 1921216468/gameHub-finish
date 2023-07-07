54b227f6512f69b64b037d34ac81ea31d184d4e8
早点睡觉 <1921216468@qq.com>
2023-07-08 00:10:05 +0800

    new  travel

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "ccf9834d61e74841a66dab39b2f94b8b",
   },
 });
